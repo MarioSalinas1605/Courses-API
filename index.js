25f8887d593c6280cfc8d2e88a8f33f46ff8bd02
Mario Salinas <mario_1605@live.com.mx>
2020-04-29 07:54:13 -0500

    refactor/struture of project in differents files

--- index.js
+++ index.js
@@ -1,28 +1,25 @@
 'use strict'
 
-const { graphql, buildSchema } = require('graphql')
+const { buildSchema } = require('graphql')
 const express = require('express')
 const gplMiddleware = require('express-graphql')
+const { readFileSync } = require('fs')
+const { join } = require('path')
+const resolvers = require('./lib/resolvers')
 
 const app = express()
 const port = process.env.port || 3030
 
-const schema = buildSchema(`
-    type Query {
-        hello: String
-    }
-`)
-
-const resolvers = {
-    hello: () => 'hola mundo'
-}
+const schema = buildSchema(
+  readFileSync(join(__dirname, 'lib', 'schema.graphql'), 'utf-8')
+)
 
 app.use('/api', gplMiddleware({
-    schema,
-    rootValue: resolvers,
-    graphiql: true
+  schema,
+  rootValue: resolvers,
+  graphiql: true
 }))
 
 app.listen(port, () => {
-    console.log(`Server is listening at http://localhost:${port}/api`);
+  console.log(`Server is listening at http://localhost:${port}/api`)
 })
