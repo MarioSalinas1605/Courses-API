22fb43d5c567fc3d2fcaff0737758e2feb6e8a5a
Mario Salinas <mario_1605@live.com.mx>
2020-05-28 13:19:04 -0500

    feature/cors and production environment

--- config/index.js
+++ config/index.js
@@ -5,7 +5,8 @@ const config = {
     password: process.env.DB_PASSWORD,
     host: process.env.DB_HOST,
     name: process.env.DB_NAME,
-    port: process.env.PORT
+    port: process.env.PORT,
+    node_env: process.env.NODE_ENV 
 }
 
 module.exports = config
--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 const config = require('./config/index')
 const { makeExecutableSchema } = require('graphql-tools')
 const express = require('express')
+const cors = require('cors')
 const gplMiddleware = require('express-graphql')
 const { readFileSync } = require('fs')
 const { join } = require('path')
@@ -10,14 +11,17 @@ const resolvers = require('./lib/resolvers')
 
 const app = express()
 const port = config.port || 3030
+const isDev = config.node_env !== 'production'
 
 const typeDefs = readFileSync(join(__dirname, 'lib', 'schema.graphql'), 'utf-8')
 
 const schema = makeExecutableSchema({typeDefs, resolvers})
 
+app.use(cors())
+
 app.use('/api', gplMiddleware({
   schema,
-  graphiql: true
+  graphiql: isDev
 }))
 
 app.listen(port, () => {
